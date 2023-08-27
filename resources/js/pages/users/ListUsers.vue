4cc4b106052d5e077e3737a5b79aad5df00c8825
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-08-27 12:59:50 +0300

    10

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -3,7 +3,9 @@
 import {onMounted, reactive, ref} from "vue";
 import {Form, Field} from "vee-validate";
 import * as yup from 'yup';
+import { useToastr } from "@/toastr.js";
 
+const toastr = useToastr()
 const users = ref([])
 const editing = ref(false)
 const formValues = ref()
@@ -42,6 +44,7 @@ const createUser = (values, {resetForm, setErrors}) => {
             users.value.unshift(response.data)
             $('#userFormModal').modal('hide')
             resetForm()
+            toastr.success('User created successfully!!!')
         })
         .catch((error) => {
             if (error.response.data.errors) {
@@ -67,6 +70,7 @@ const updateUser = (values, { setErrors }) => {
             const index = users.value.findIndex(user => user.id === response.data.id)
             users.value[index] = response.data
             $('#userFormModal').modal('hide')
+            toastr.success('User updated successfully!!!')
         })
         .catch(error => {
             setErrors(error.response.data.errors)
