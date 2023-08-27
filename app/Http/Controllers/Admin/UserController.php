59254b2b57b873cb9ca7c7fe6cdbde731a7f1bae
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-08-27 22:14:24 +0300

    12

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -45,4 +45,10 @@ class UserController extends Controller
 
         return $user;
     }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->noContent();
+    }
 }
--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -10,6 +10,7 @@ const users = ref([])
 const editing = ref(false)
 const formValues = ref()
 const form = ref(null)
+const userIdBeingDeleted = ref(null)
 
 const getUsers = () => {
     axios.get('/api/users')
@@ -78,6 +79,20 @@ const updateUser = (values, { setErrors }) => {
 
 }
 
+const confirmUserDelete = (user) => {
+    userIdBeingDeleted.value = user.id
+    $('#deleteUserModal').modal('show')
+}
+
+const deleteUser = () => {
+    axios.delete(`/api/users/${userIdBeingDeleted.value}`)
+        .then(() => {
+            $('#deleteUserModal').modal('hide')
+            users.value = users.value.filter(user => user.id !== userIdBeingDeleted.value)
+            toastr.success('User deleted successfully!!!')
+        })
+}
+
 const handleSubmit = (values, actions) => {
     if (editing.value) {
         updateUser(values, actions)
@@ -137,6 +152,7 @@ onMounted(() => {
                             <td>-</td>
                             <td>
                                 <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
+                                <a href="#" @click.prevent="confirmUserDelete(user)"><i class="fa fa-trash text-danger ml-2"></i></a>
                             </td>
                         </tr>
                         </tbody>
@@ -215,6 +231,32 @@ onMounted(() => {
                         <button type="submit" class="btn btn-primary">Save</button>
                     </div>
                 </Form>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="deleteUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+         aria-labelledby="deleteUserModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteUserModalLabel">
+                        <span>Delete User</span>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <h5>Are you sure you want to delete this user?</h5>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-danger" @click.prevent="deleteUser">Delete User</button>
+                </div>
+
             </div>
         </div>
     </div>
