67553f25552e04c3553ca62181fb975a1da1124b
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-08-27 14:37:32 +0300

    11

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -17,7 +17,9 @@ class UserController extends Controller
     public function store()
     {
         request()->validate([
-            'email' => 'required|unique:users,email'
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required|min:8'
         ]);
 
         return User::create([
@@ -30,7 +32,9 @@ class UserController extends Controller
     public function update(User $user)
     {
         request()->validate([
-            'email' => 'required|unique:users,email,' . $user->id
+            'name' => 'required',
+            'email' => 'required|unique:users,email,' . $user->id,
+            'password' => 'sometimes|min:8'
         ]);
 
         $user->update([
