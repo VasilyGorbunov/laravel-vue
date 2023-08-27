6fa237f4c38e41e54ec7fdff2e4e91e3e1d3a6e5
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-08-27 12:28:27 +0300

    08

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -5,6 +5,9 @@ import {Form, Field} from "vee-validate";
 import * as yup from 'yup';
 
 const users = ref([])
+const editing = ref(false)
+const formValues = ref()
+const form = ref(null)
 
 const getUsers = () => {
     axios.get('/api/users')
@@ -13,19 +16,67 @@ const getUsers = () => {
         })
 }
 
-const schema = yup.object({
+const createUserSchema = yup.object({
     name: yup.string().required(),
     email: yup.string().email().required(),
     password: yup.string().required().min(8),
 })
 
-const createUser = (values, { resetForm }) => {
+const editUserSchema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().when((password, schema) => {
+        return password ? schema.required().min(8) : schema
+    })
+})
+
+const addUser = () => {
+    editing.value = false
+    form.value.resetForm()
+    $('#userFormModal').modal('show')
+}
+
+const createUser = (values) => {
     axios.post('/api/users', values)
         .then(response => {
             users.value.unshift(response.data)
-            $('#createUserModal').modal('hide')
-            resetForm()
+            $('#userFormModal').modal('hide')
+            form.value.resetForm()
         })
+}
+
+const editUser = (user) => {
+    editing.value = true
+    form.value.resetForm()
+    $('#userFormModal').modal('show')
+    formValues.value = {
+        id: user.id,
+        name: user.name,
+        email: user.email
+    }
+}
+
+const updateUser = (values) => {
+    axios.put('/api/users/' + formValues.value.id, values)
+        .then(response => {
+            const index = users.value.findIndex(user => user.id === response.data.id)
+            users.value[index] = response.data
+            $('#userFormModal').modal('hide')
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        .finally(() => {
+            form.value.resetForm()
+        })
+}
+
+const handleSubmit = (values) => {
+    if (editing.value) {
+        updateUser(values)
+    } else {
+        createUser(values)
+    }
 }
 
 onMounted(() => {
@@ -53,7 +104,7 @@ onMounted(() => {
 
     <div class="content">
         <div class="container-fluid">
-            <button type="button" class="mb-2 btn btn-primary" data-toggle="modal" data-target="#createUserModal">
+            <button @click="addUser" type="button" class="mb-2 btn btn-primary">
                 Add New User
             </button>
 
@@ -77,7 +128,9 @@ onMounted(() => {
                             <td>{{ user.email }}</td>
                             <td>-</td>
                             <td>-</td>
-                            <td>-</td>
+                            <td>
+                                <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
+                            </td>
                         </tr>
                         </tbody>
                     </table>
@@ -86,17 +139,26 @@ onMounted(() => {
         </div>
     </div>
 
-    <div class="modal fade" id="createUserModal" data-backdrop="static" tabindex="-1" role="dialog"
-         aria-labelledby="createUserModalLabel" aria-hidden="true">
+    <div class="modal fade" id="userFormModal" data-backdrop="static" tabindex="-1" role="dialog"
+         aria-labelledby="userFormModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="createUserModalLabel">Add New User</h5>
+                    <h5 class="modal-title" id="userFormModalLabel">
+                        <span v-if="editing">Edit User</span>
+                        <span v-else>Add New User</span>
+                    </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <Form @submit="createUser" :validation-schema="schema" v-slot="{ errors }">
+                <Form
+                    ref="form"
+                    @submit="handleSubmit"
+                    :validation-schema="editing ? editUserSchema : createUserSchema"
+                    v-slot="{ errors }"
+                    :initial-values="formValues"
+                >
                     <div class="modal-body">
                         <div class="form-group">
                             <label for="name">Name</label>
